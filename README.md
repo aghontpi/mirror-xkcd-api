9a2d1feaf4c67c5a9946c13e79c6ecd996347fdc
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2020-12-04 16:20:46 +0530

    :pencil2: automated sync

---
+++ README.md
@@ -0,0 +1,47 @@
+# Mirror of xkcd api
+
+> Update-to-date mirror of xkcd api, which Supports CORS.
+
+## Mirror for xkcd
+
+This repository will automatically update with new content everyday.
+
+
+## How to use
+
+***to consume json for x/your_value***
+
+```text
+https://raw.githubusercontent.com/aghontpi/mirror-xkcd-api/master/api/{x}/info.0.json
+```
+
+***example: make a request to 190***
+
+```text
+https://raw.githubusercontent.com/aghontpi/mirror-xkcd-api/master/api/190/info.0.json
+```
+
+### Making request will give the following response
+
+```json
+{
+  "month": "11",
+  "num": 190,
+  "link": "",
+  "year": "2006",
+  "news": "",
+  "safe_title": "IPoD",
+  "transcript": "[[Character 1 - wearing a black hat - sits at a computer. Character 2 stands behind Character 1]]\nCharacter 1: You see, statisticians communicate using IPoD -- IP over Demographics. For example, the header of the next packet I send will be encoded into the New Jersey death rate.\nCharacter 2: So you're going to hack the census bureau and change the number of reported deaths?\nCharacter 1: Guess again.\nCharacter 1: Hey, have you seen my crossbow?\n{{Alt: For smaller numbers he has to SAVE lives.  The birthrate channel is even more of a mixed bag.}}",
+  "alt": "For smaller numbers he has to SAVE lives.  The birthrate channel is even more of a mixed bag.",
+  "img": "https://imgs.xkcd.com/comics/ipod.png",
+  "title": "IPoD",
+  "day": "29",
+  "mirror_img": "https://raw.githubusercontent.com/aghontpi/mirror-xkcd-api/main/api/190/ipod.png"
+}
+```
+
+### Parse the json and use value of "mirror_img" instead of "img"
+
+'img': 'https://imgs.xkcd.com/comics/ipod.png' is the original link, 
+
+You should use, "mirror_img": "https://raw.githubusercontent.com/aghontpi/mirror-xkcd-api/main/api/190/ipod.png"
---
+++ sync.py
@@ -0,0 +1,161 @@
+#!/usr/bin/python3
+
+# @author: aghontpi
+# File is run by github-action everyday and -
+#  - syncs content with api
+
+from pathlib import Path
+import json
+import requests
+import datetime
+
+
+class Utils(object):
+    def __init__(self):
+        self.offline_directory = Path(__file__).parent
+        self.download_directory = "api"
+        self.log_directory = "logs"
+        self.state_filename = "syncState.json"
+        self._log(
+            f"\n{'*'*20} {datetime.datetime.now().strftime('%Y-%m-%d')} {'*'*20}")
+
+    def _saveState(self, json_content):
+        self._log("updating syncState", json_content)
+        filepath = self.offline_directory.joinpath(self.state_filename)
+        with open(filepath, 'w') as writefile:
+            json.dump(json_content, writefile)
+        writefile.close()
+
+    def _log(self, log, _arg=''):
+        filepath = self.offline_directory.joinpath(self.log_directory)
+        log_string = f"{log} {str(_arg)}"
+        if not Path.is_dir(filepath):
+            Path(filepath).mkdir()
+            log_string = f"creating logs path\n" + log_string
+        filepath = filepath.joinpath('log.txt')
+        with open(filepath, 'a+') as logFile:
+            logFile.write(
+                f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} {log_string} \n")
+        logFile.close()
+        print(log_string)
+
+    def _saveContent(self, json_content):
+        id = json_content['num']
+        img_url = json_content['img']
+        img_name = img_url.split('/')[-1]
+        # handle no image for a specific json
+        if img_name == "":
+            self._log("no image for :", id)
+            return True
+        download_directory = self.offline_directory.joinpath(
+            self.download_directory)
+        if not Path.is_dir(download_directory):
+            try:
+                Path(download_directory).mkdir()
+            except:
+                self._log("can not create api directory")
+                return False
+            self._log("saving in new directory :",
+                      download_directory.as_posix())
+        download_directory = download_directory.joinpath(str(id))
+        if not Path.is_dir(download_directory):
+            try:
+                Path(download_directory).mkdir()
+            except:
+                self._log("can not create offline directory")
+                return False
+            self._log("saving in new directory : ",
+                      download_directory.as_posix())
+
+        # save assests
+        img = requests.get(img_url)
+        if img.status_code != 200:
+            self._log("error downloading image :", img_url)
+            return False
+        file_path = download_directory.joinpath(img_name)
+        with open(file_path, 'wb') as save_file:
+            save_file.write(img.content)
+        save_file.close()
+
+        # saving the info.json
+        file_path = download_directory.joinpath('info.0.json')
+        json_content[
+            'mirror_img'] = f'https://raw.githubusercontent.com/aghontpi/mirror-xkcd-api/main/api/{id}/{img_name}'
+        with open(file_path, 'w') as save_file:
+            json.dump(json_content, save_file)
+        save_file.close()
+        return True
+
+
+class Sync(Utils):
+
+    def __init__(self):
+        self.urllink = 'https://xkcd.com/info.0.json'
+        self.local_content = None
+        self.remote_content = None
+        super(Sync, self).__init__()
+
+    def sync(self):
+        if self._parseLocalstate()._parseRemoteState()._compareLocalAndRemote():
+            self._log('no new content to sync')
+            exit()
+        self.downloadNewContent()
+
+    def _parseLocalstate(self):
+        filepath = self.offline_directory.joinpath(self.state_filename)
+        if not Path.is_file(filepath):
+            # not present, creating file
+            self._log('creating local state file ')
+            with open(filepath, 'w') as create_file:
+                create_file.write('{"last_update_content": {"id": "1"}}')
+            create_file.close()
+
+        with open(filepath, encoding='utf-8') as json_file:
+            data = json.loads(json_file.read())
+        json_file.close()
+        self.local_content = data
+        return self
+
+    def _parseRemoteState(self):
+        try:
+            remoteContent = requests.get(self.urllink).content
+            remoteContent = (json.loads(remoteContent))
+        except:
+            pass
+        self.remote_content = remoteContent
+        return self
+
+    def _compareLocalAndRemote(self):
+        l_content_id = int(self.local_content['last_update_content']['id'])
+        r_content_id = int(self.remote_content['num'])
+        return not l_content_id <= r_content_id
+
+    def downloadNewContent(self):
+        l_content = self.local_content
+        r_content = self.remote_content
+        # check local content id vs remote content id
+        self._log("processing", l_content['last_update_content']['id'])
+        l_content_id = int(l_content['last_update_content']['id'])
+        r_content_id = int(r_content['num'])
+        while l_content_id < r_content_id:
+            try:
+                json_content = json.loads(requests.get(
+                    "https://xkcd.com/"+str(l_content_id)+"/info.0.json").content)
+            except:
+                self._log('can not contact remote for :', l_content_id)
+                self._log('skipping ', l_content_id)
+                l_content_id += 1
+                continue
+            if self._saveContent(json_content):
+                l_content_id += 1
+                l_content['last_update_content']['id'] = str(l_content_id)
+                self._log('downloaded content :', l_content_id)
+                self._saveState(self.local_content)
+            else:
+                self._log('error saving content... skipping')
+                break
+
+
+if __name__ == "__main__":
+    sync = Sync()
+    sync.sync()
